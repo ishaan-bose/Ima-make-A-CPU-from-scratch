4af238cea0fdfa1f72afcd8280f23731cfc35e7d
ishaan-bose <ishaan.varshney.bose@gmail.com>
2020-12-01 17:57:29 +0530

    this is the adder file in verilog that has a half adder and a full adder

--- arithmetic/Adder.v
+++ arithmetic/Adder.v
@@ -1,25 +1,100 @@
-module half_adder (in1,in2,sum,carry);
+module half_adder (in1,
+                    in2,
+                    sum,
+                    carry);
    
    input in1, in2;
    output sum, carry;
 
-   assign sum = in1 ^ in2; 
-   assign carry = in1 & in2;
+   assign sum = in1 ^ in2;
+   assign carry = in1 && in2;
    
    
 endmodule // half_adder
 
-module full_adder (in1,in2,carryIn,sum,carryOut);
+module full_adder (in1,
+                    in2,
+                    carryIn,
+                    sum,
+                    carryOut);
 
     input in1,in2,carryIn;
     output sum,carryOut;
     
-    wire operandSum, operandCarry;
+    wire InputSum, InputCarry;
     wire finalSum, finalCarry;
     
-    half_adder a(in1, in2, operandSum, operandCarry);
-    half_adder b(operandSum, carryIn, finalSum, finalCarry);
+    half_adder a(in1, in2, InputSum, InputCarry);
+    half_adder b(InputSum, carryIn, finalSum, finalCarry);
     
-    assign carryOut = finalCarry | operandCarry;
+    assign carryOut = finalCarry || InputCarry;
+    assign sum = finalSum;
 
 endmodule // full_adder
+
+
+
+
+
+
+
+
+// testbench
+module testbench;
+
+    reg A, B, Cin;
+    wire S, Cout;
+
+    full_adder a(A, B, Cin ,S, Cout);
+
+initial begin
+    
+    A = 0; B = 0; Cin = 0;
+
+    #10;
+    A = 0;
+    B = 0;
+    Cin = 0;
+    
+    #10
+    A = 0;
+    B = 0;
+    Cin = 1;
+
+    #10
+    A = 0;
+    B = 1;
+    Cin = 0;
+
+    #10
+    A = 0;
+    B = 1;
+    Cin = 1;
+
+    #10;
+    A = 1;
+    B = 0;
+    Cin = 0;
+    
+    #10
+    A = 1;
+    B = 0;
+    Cin = 1;
+
+    #10
+    A = 1;
+    B = 1;
+    Cin = 0;
+
+    #10
+    A = 1;
+    B = 1;
+    Cin = 1;
+    
+end
+
+initial begin
+    $monitor("A = %d, B = %d, Cin = %d ,S = %d, Cout = %d", A, B, Cin, S, Cout);
+end
+
+endmodule // testbench
